08eefe062174050ab15f5f662a34f3a52f60c89f
TechPriest <yaminogakusei@gmail.com>
2010-09-30 13:55:09 +0300

    A bunch of refactoring.

--- js/node_map.js
+++ js/node_map.js
@@ -15,7 +15,7 @@ var node_map = {
     // Load markers of needed type
     var type = jQuery('#node_map').attr('data-node-type');
     jQuery.ajax({
-      url: '/node_map_callback/' + type,
+      url: '/node_map_callback/markers/' + type,
       dataType: 'json',
       error: function(request, status, exception) {
         alert('Failed to load map data');
@@ -30,25 +30,36 @@ var node_map = {
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0; i < data.length; i++) {
       var d = data[i];
-      var markerOpts = {
-        title: d.title,
-        position: new google.maps.LatLng(d.latitude, d.longitude),
-        map: node_map.map
-      };
-      if (d.marker) {
-        markerOpts.icon = new google.maps.MarkerImage(d.marker.path + d.marker.iconfile,
-            undefined, undefined, new google.maps.Point(d.marker.anchorX, d.marker.anchorY));
-        if (d.marker.shadow) {
-          markerOpts.shadow = new google.maps.MarkerImage(d.marker.path + d.marker.shadow,
-            undefined, undefined, new google.maps.Point(d.marker.anchorX, d.marker.anchorY));
-        }
-      }
-      var marker = new google.maps.Marker(markerOpts); 
+      var marker = node_map.processMarker(d);
       bounds.extend(marker.position);
     }
     // fit all the markers to the map
     node_map.map.fitBounds(bounds);
     node_map.map.setZoom(Math.min(17, node_map.map.getZoom()));
+  },
+  
+  processMarker: function(d) {
+    var markerOpts = {
+      title: d.title,
+      position: new google.maps.LatLng(d.latitude, d.longitude),
+      map: node_map.map
+    };
+    if (d.marker) {
+      markerOpts.icon = new google.maps.MarkerImage(d.marker.path + d.marker.iconfile,
+          undefined, undefined, new google.maps.Point(d.marker.anchorX, d.marker.anchorY));
+      if (d.marker.shadow) {
+        markerOpts.shadow = new google.maps.MarkerImage(d.marker.path + d.marker.shadow,
+          undefined, undefined, new google.maps.Point(d.marker.anchorX, d.marker.anchorY));
+      }
+    }
+    var marker = new google.maps.Marker(markerOpts);
+    google.maps.event.addListener(marker, 'click', function() {
+      var infoWindow = new google.maps.InfoWindow({
+        content: '<strong>Displaying information for node ' + d.nid + '</strong>'
+      });
+      infoWindow.open(node_map.map, marker);
+    });
+    return marker;
   }
 }
 
